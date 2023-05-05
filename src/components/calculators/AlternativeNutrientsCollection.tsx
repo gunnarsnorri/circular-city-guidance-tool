2117bc8fa174ae4f82776ddbd8c90aabf543c59a
Gunnar Snorri Ragnarsson <gunnar.snorri.ragnarsson@gmail.com>
2023-05-05 16:09:56 +0200

    change monthly alternative nutrients collection calculator to daily/monthly/yearly

--- src/components/calculators/AlternativeNutrientsCollection.tsx
+++ src/components/calculators/AlternativeNutrientsCollection.tsx
@@ -1,24 +1,35 @@
 import Stack from "react-bootstrap/Stack";
-import { Form } from "react-bootstrap";
-import MonthlyTable from "../MonthlyTable";
+import { Form, Table } from "react-bootstrap";
 import { allRegionData } from "../../calculator/Region";
 import { GlobalCalcStorage } from "../../interfaces/CalculatorInterface";
 import PersonForm from "../PersonForm";
 
 export default function AlternativeNutrientsCollectionCalculator({ globalStorage, setGlobalStorage }: { globalStorage: Partial<GlobalCalcStorage>, setGlobalStorage: Function }) {
-    const monthlyBlackWater = Array<number>(12);
-    const monthlyYellowWater = Array<number>(12);
-    const monthlyBrownWater = Array<number>(12);
+    let dailyBlackWater: number | undefined = undefined;
+    let dailyYellowWater: number | undefined = undefined;
+    let dailyBrownWater: number | undefined = undefined;
+    let monthlyBlackWater: number | undefined = undefined;
+    let monthlyYellowWater: number | undefined = undefined;
+    let monthlyBrownWater: number | undefined = undefined;
+    let yearlyBlackWater: number | undefined = undefined;
+    let yearlyYellowWater: number | undefined = undefined;
+    let yearlyBrownWater: number | undefined = undefined;
     if (globalStorage.region !== undefined && allRegionData[globalStorage.region] !== undefined) {
         const regionData = allRegionData[globalStorage.region];
         if (regionData.blackWater !== undefined) {
-            monthlyBlackWater.fill(30 * (globalStorage.persons ?? 0) * regionData.blackWater);
+            dailyBlackWater = (globalStorage.persons ?? 0) * regionData.blackWater;
+            monthlyBlackWater = 30 * dailyBlackWater;
+            yearlyBlackWater = 365 * dailyBlackWater;
         }
         if (regionData.yellowWater !== undefined) {
-            monthlyYellowWater.fill(30 * (globalStorage.persons ?? 0) * regionData.yellowWater);
+            dailyYellowWater = (globalStorage.persons ?? 0) * regionData.yellowWater;
+            monthlyYellowWater = 30 * dailyYellowWater;
+            yearlyYellowWater = 365 * dailyYellowWater;
         }
         if (regionData.brownWater !== undefined) {
-            monthlyBrownWater.fill(30 * (globalStorage.persons ?? 0) * regionData.brownWater);
+            dailyBrownWater = (globalStorage.persons ?? 0) * regionData.brownWater;
+            monthlyBrownWater = 30 * dailyBrownWater;
+            yearlyBrownWater = 365 * dailyBrownWater;
         }
     }
     return (
@@ -28,13 +39,36 @@ export default function AlternativeNutrientsCollectionCalculator({ globalStorage
                     <PersonForm globalStorage={globalStorage} setGlobalStorage={setGlobalStorage} />
                 </Form.Group>
             </Form>
-            <MonthlyTable columns={
-                [
-                    { valueName: "Black Water", unit: "L", monthlyValues: monthlyBlackWater },
-                    { valueName: "Yellow Water", unit: "L", monthlyValues: monthlyYellowWater },
-                    { valueName: "Brown Water", unit: "L", monthlyValues: monthlyBrownWater }
-                ]
-            } />
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Black Water (L)</th>
+                        <th>Yellow Water (L)</th>
+                        <th>Brown Water (L)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Daily</td>
+                        <td>{dailyBlackWater}</td>
+                        <td>{dailyYellowWater}</td>
+                        <td>{dailyBrownWater}</td>
+                    </tr>
+                    <tr>
+                        <td>Monthly</td>
+                        <td>{monthlyBlackWater}</td>
+                        <td>{monthlyYellowWater}</td>
+                        <td>{monthlyBrownWater}</td>
+                    </tr>
+                    <tr>
+                        <td>Yearly</td>
+                        <td>{yearlyBlackWater}</td>
+                        <td>{yearlyYellowWater}</td>
+                        <td>{yearlyBrownWater}</td>
+                    </tr>
+                </tbody>
+            </Table>
         </Stack>
     )
 }
